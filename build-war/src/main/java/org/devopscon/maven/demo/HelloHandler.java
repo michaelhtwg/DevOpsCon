fe8d0f288a5e8c198ed106457126c077391877a5
michael <michaelkellermeier@gmx.de>
2018-05-28 11:34:32 +0200

    initial DevOpsCommit

---
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -0,0 +1,10 @@
+package org.devopscon.maven.demo;
+
+public class HelloHandler {
+
+	public String sayHello() {
+		String hello = "Hi man? What do you think about this WAR file?";
+		return hello;
+	}
+
+}
